78dda49815039bdb3f656803b2c339533b2f2dde
Jazz <102905413+jazz-design@users.noreply.github.com>
2023-02-02 16:32:23 +0530

    updations

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -7,9 +7,9 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Apptitude", "Interest"],
     datasets: [{
-      data: [55, 30, 15],
+      data: [65, 35],
       backgroundColor: ['#571386', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
